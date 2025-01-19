69d8a8852d958029f053abf3f11c81e3a36edd9e
Gidd-X <151777807+Gidd-X@users.noreply.github.com>
2025-01-19 14:59:14 +0100

    Customer Churn Model

--- work_1/script.js
+++ work_1/script.js
@@ -1,10 +1,6 @@
 const btn = document.querySelector('#btn');
 const btn_text = document.querySelector('#btn_text');
 
-let count = 1
-
-btn.onclick = () => {
-    btn.textContent = "Try again!";
-    btn_text.textContent = `${count} clicks so far`;
-    count += 1;
-  };
+btn.addEventListener('click', () => {
+  btn_text.innerHTML = 'Thank you for your interest';
+})
